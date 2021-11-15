8ed67f62da3d843966b4ee42197b8dd7581fb817
Patrick-InfHo <patrick.haetti@infho.eu>
2021-11-15 17:40:14 +0100

    add input field

--- vokabel_trainer_v_002.py
+++ vokabel_trainer_v_002.py
@@ -17,20 +17,32 @@ window.geometry('800x600')
 frm = ttk.Frame(window, padding=10)
 frm.grid()
 
-def display_new_exercise():
-    print("test erfolgreich")
-    anzeige.configure(text=str(random.randint(1,10)))
 
-ttk.Label(frm, text="Hello!").grid(column=1, row=0)
+def display_new_exercise():
+    global y
+    y = random.choice(list(words.keys()))
+    anzeige.configure(text=str(y))
+    print(y, ">>>>", words[y])
+
+
+#ttk.Label(frm, text="Hello!").grid(column=1, row=0)
 anzeige = ttk.Label(frm, text="Hallo!")
 anzeige.grid(column=2, row=0)
 ttk.Button(frm, text="Quit", command=window.destroy).grid(column=5, row=0)
-ttk.Label(frm, text=words).grid(column=3, row=0)
+#ttk.Label(frm, text=words).grid(column=3, row=0)
 ttk.Button(frm, text="Neue Aufgabe", command=display_new_exercise).grid(column=4, row=0)
 
+e = Entry(frm)
+e.grid(row=0, column=6)
 
+def testfunktion():
+    print(e.get())
+    print("^^^",words[y],y)
+    if e.get() == words[y]:
+        print("richtig")
+    else:
+        print("falsch")
 
-
-e = Entry(frm).grid(row=0, column=6)
+ttk.Button(frm, text="test", command=testfunktion).grid(column=0,row=0)
 
 window.mainloop()
